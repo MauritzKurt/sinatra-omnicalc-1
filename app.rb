1da36056081fb8113faa88e1fae1ad7d07cead88
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-06-03 04:52:00 +0000

    Finished 2/4 forms

--- app.rb
+++ app.rb
@@ -1,6 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  @square_num = params.fetch("users_number").to_f
+  @square_result = @square_num ** 2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @square_root_num = params.fetch("users_number").to_f
+  @square_root_result = @square_root_num ** 0.5
+  erb(:square_root_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
